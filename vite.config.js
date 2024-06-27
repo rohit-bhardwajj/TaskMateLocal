1f9f0b5a397392e5ce6e8e1e9683fb79f6aa1d95
Rohit kumar <rohitkumar01498@gmail.com>
2024-06-27 19:31:37 +0530

    change1

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Ensure this is the directory Netlify expects
+  },
   base: '/TaskMateLocal/',
 })
